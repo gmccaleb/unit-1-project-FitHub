c103cea6280ae0b5b2c353132d6deb64c274d908
gmccaleb <gabriellemccaleb@gmail.com>
2025-11-03 20:43:30 -0600

    started on Log Workout component, all pages are linked so far

--- fit-hub/src/components/LogWorkout.jsx
+++ fit-hub/src/components/LogWorkout.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function LogWorkout() {
-    return (
-       <div>
-
-       </div> 
-    )
+  return (
+    <main>
+      <h2>Log a Workout</h2>
+      <form>
+        <label>
+          {" "}
+          Workout Title
+          <input type="text"/>
+        </label>
+        <h4>Exercises:</h4>
+        <input type="text" placeholder="Exercise Name" required />
+        <input type="number" placeholder="Sets" />
+        <input type="number" placeholder="Reps" />
+      </form>
+    </main>
+  );
 }
 
-export default LogWorkout
+export default LogWorkout;
